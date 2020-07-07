e5fb858201a6e20051e9828bcf18407d69a26b29
dana6691 <dahee.kim@mnsu.edu>
2020-07-07 18:01:33 -0500

    Update beautifulsoup.py

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -57,4 +57,3 @@ products=[] #List to store name of the product
 prices=[] #List to store price of the product
 ratings=[] #List to store rating of the product
 driver.get("https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&amp;amp;amp;uniq")
-# %%
